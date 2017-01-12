53c3827bc2b2596138e48f64953cb9d078b15e15
DalinXie <xdl.tmac@qq.com>
2017-01-11 19:38:27 -0500

    updateVersonXMPP

---
+++ DLXMPPFrame/DLXMPPFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLXMPPFrame
+//
+//  Created by DalinXie on 17/1/11.
+//  Copyright © 2017年 itdarlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
